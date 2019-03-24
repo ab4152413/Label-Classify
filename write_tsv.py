55a54848e781211f0d23a439ee37b589eeb1090d
Gan1aa <34432717+ab4152413@users.noreply.github.com>
2019-03-24 13:11:43 +0800

    Update write_tsv.py

--- write_tsv.py
+++ write_tsv.py
@@ -1,17 +1,17 @@
-#encoding=utf-8
+# encoding=utf-8
 import jieba
 import fasttext
 
-classifier=fasttext.load_model('nwelab3fenci.model.bin',label_prefix='__label__')
+classifier = fasttext.load_model('nwelab3fenci.model.bin', label_prefix='__label__')
 
-ff = open("data/test2.tsv",'w') #打开一个文件可写模式
-with open("data/test.tsv",'r') as f:  #打开一个文件只读模式
+ff = open("data/test2.tsv", 'w')  # 打开一个文件可写模式
+with open("data/test.tsv", 'r') as f:  # 打开一个文件只读模式
     next(f)
     line = f.readlines()
     for line_list in line:
-        line_new =line_list.replace('\n','') #将换行符替换为空('')
-        seg_name = jieba.cut(line_new) #jieba分词处理
-        outline = classifier.predict([" ".join(seg_name)]) #构成字符串 传入分类器进行预测
-        line_new=line_new+'\t'+outline[0][0]+'\n'  #添加预测得到的标签,同时加上"\n"换行符
-        print(line_new) #控制台显示
-        ff.write(line_new) #写入一个新文件中
+        line_new = line_list.replace('\n', '')  # 将换行符替换为空('')
+        seg_name = jieba.cut(line_new)  # jieba分词处理
+        outline = classifier.predict([" ".join(seg_name)])  # 构成字符串 传入分类器进行预测
+        line_new = line_new + '\t' + outline[0][0] + '\n'  # 添加预测得到的标签,同时加上"\n"换行符
+        print(line_new)  # 控制台显示
+        ff.write(line_new)  # 写入一个新文件中
